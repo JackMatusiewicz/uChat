09f0c86cce7192e151d08b894f6181e854cc1b36
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2023-12-15 23:35:30 +0000

    Proof of concept of the UDP multicast working in rust via tokio and socket2

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+
+use lazy_static::lazy_static;
+
+use socket2::{Domain, Protocol, SockAddr, Socket, Type};
+use std::{net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4}, sync::{atomic::AtomicBool, Arc}, io, mem::{MaybeUninit, ManuallyDrop}, time::Duration};
+
+static PORT: u16 = 7982;
+
+lazy_static! {
+    pub static ref IPV4: IpAddr = Ipv4Addr::new(224, 0, 0, 69).into();
+}
+
+fn main() {
+    let is_finished = Arc::new(AtomicBool::new(false));
+    let read_handle = is_finished.clone();
+    let write_handle = is_finished.clone();
+
+    let runtime = tokio::runtime::Builder::new_current_thread().thread_name("read").enable_io().build().unwrap();
+    let runtime2 = tokio::runtime::Builder::new_current_thread().thread_name("write").enable_io().build().unwrap();
+    let read_handle = std::thread::spawn(move || {
+        runtime.block_on(async move {
+            let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP)).unwrap();
+            socket.join_multicast_v4(&Ipv4Addr::new(224, 0, 0, 69), &Ipv4Addr::UNSPECIFIED).unwrap();
+            socket.bind(&SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, PORT).into()).unwrap();
+    
+            let udp_socket = tokio::net::UdpSocket::from_std(socket.into()).unwrap();
+    
+            let mut buf: [u8; 50] = [0;50];
+            while !read_handle.load(std::sync::atomic::Ordering::Relaxed) {
+                match udp_socket.recv_from(&mut buf).await {
+                    Ok(_) => {
+                        let value = String::from_utf8_lossy(&buf);
+                        println!("{value}");
+                    },
+                    _ => {
+    
+                    }
+                }
+            }
+        });
+    });
+
+    let write_jh = std::thread::spawn(move || {
+        runtime2.block_on(async move {
+            let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP)).unwrap();
+            socket.set_multicast_if_v4(&Ipv4Addr::UNSPECIFIED).unwrap();
+            socket.bind(&SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0).into()).unwrap();
+    
+    
+            let udp_socket = tokio::net::UdpSocket::from_std(socket.into()).unwrap();
+            let mut ctr = 1;
+            while !write_handle.load(std::sync::atomic::Ordering::Relaxed) {
+                let to_send = format!("Jack has {} braincells", ctr);
+                let bytes = to_send.as_bytes();
+                udp_socket.send_to(&bytes, &SocketAddrV4::new(Ipv4Addr::new(224, 0, 0, 69), PORT)).await.unwrap();
+                std::thread::sleep(Duration::from_secs(2));
+                ctr += 1;
+            }
+        })
+    });
+
+
+    loop {
+        let mut str = String::new();
+        let v = std::io::stdin().read_line(&mut str);
+        match v {
+            Ok(_) => {
+                if str == "end" {
+                    println!("Program will end");
+                    is_finished.store(true, std::sync::atomic::Ordering::Relaxed);
+                    break;
+                }
+            },
+            _ => {}
+        }
+    }
+}
