28d0dac586825c65cc6012ad17cb48ea6532607b
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2023-12-16 13:39:42 +0000

    Adding checks that send + receive are ready before we start looping

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let is_finished = Arc::new(AtomicBool::new(false));
     let read_handle = is_finished.clone();
     let write_handle = is_finished.clone();
+    let reader_ready = Arc::new(AtomicBool::new(false));
+    let rr = reader_ready.clone();
+    let writer_ready = Arc::new(AtomicBool::new(false));
+    let wr = writer_ready.clone();
 
     let read_jh = tokio::spawn(async move {
         let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP)).unwrap();
@@ -34,6 +38,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             bind_socket_multicast(&socket, &sa).unwrap();
             socket.set_read_timeout(Some(Duration::from_millis(200))).unwrap();
             let udp_socket = tokio::net::UdpSocket::from_std(socket.into()).unwrap();
+            reader_ready.store(true, std::sync::atomic::Ordering::Relaxed);
     
             let mut buf: [u8; 50] = [0;50];
             while !read_handle.load(std::sync::atomic::Ordering::Relaxed) {
@@ -54,17 +59,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
         socket.set_multicast_if_v4(&Ipv4Addr::UNSPECIFIED).unwrap();
         socket.bind(&SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0).into()).unwrap();
         socket.set_write_timeout(Some(Duration::from_millis(200))).unwrap();
-
         let udp_socket = tokio::net::UdpSocket::from_std(socket.into()).unwrap();
+        writer_ready.store(true, std::sync::atomic::Ordering::Relaxed);
+
         let mut ctr = 1;
         while !write_handle.load(std::sync::atomic::Ordering::Relaxed) {
-            let to_send = format!("Ivy has {} braincells", ctr);
+            let to_send = format!("Jack has {} braincells", ctr);
             let bytes = to_send.as_bytes();
             udp_socket.send_to(&bytes, &SocketAddrV4::new(Ipv4Addr::new(224, 0, 0, 69), PORT)).await.unwrap();
             std::thread::sleep(Duration::from_secs(2));
             ctr += 1;
         }
     });
+
+    while !rr.load(std::sync::atomic::Ordering::Relaxed) && !wr.load(std::sync::atomic::Ordering::Relaxed) {
+    }
 
     println!("Starting loop");
     loop {
